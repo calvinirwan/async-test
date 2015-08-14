5a2a11528790098aad53f58e090723746ea5bf42
calvinirwan <calvin.irwan@gmail.com>
2015-08-15 00:28:07 +0700

    mailtrap success

--- src/async_test/mailtrap.clj
+++ src/async_test/mailtrap.clj
@@ -3,4 +3,21 @@
   (:require [clj-mailgun.core :as m]
             [clojure.pprint :refer [pprint]]
             [cheshire.core :as c]
-            [clj-http.client :as client]))
+            [clj-http.client :as client]
+            [postal.core :refer [send-message] :as p]
+            [async-test.config :refer [mailtrap]]))
+
+(def conn
+  (let [{:keys [host user_name password authentication port]} mailtrap]
+    {:host host
+     :user user_name 
+     :pass password
+     :authentication authentication}))
+
+(defn send-mail
+  [email subject body]
+  (send-message conn {:from (mailtrap :user_name)
+                      :to email
+                      :subject subject
+                      :body body}))
+
